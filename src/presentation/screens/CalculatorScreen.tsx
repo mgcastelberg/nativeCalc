1b76b4b1bb58c31d53b6511d2a6ce155b0be9f23
Manuel Gomez <jmgc@virket.com>
2024-04-03 08:42:08 -0600

    Botones de calculadora

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,5 +1,6 @@
-import { Text, View } from "react-native"
-import { globalStyles } from "../../config/theme/app-theme"
+import { Pressable, Text, View } from "react-native"
+import { colors, globalStyles } from "../../config/theme/app-theme"
+import { CalculatorButton } from "../components/CalculatorButton"
 
 export const CalculatorScreen = () => {
   return (
@@ -8,6 +9,41 @@ export const CalculatorScreen = () => {
           <Text style={ globalStyles.mainResult } >1500</Text>
           <Text style={ globalStyles.subResult }>15</Text>
         </View>
+
+        <View style={ globalStyles.row }>
+          <CalculatorButton label="C" color={ colors.lightGray } blackText />
+          <CalculatorButton label="+/-" color={ colors.lightGray } blackText />
+          <CalculatorButton label="del" color={ colors.lightGray } blackText />
+          <CalculatorButton label="÷" color={ colors.orange } />
+        </View>
+
+        <View style={ globalStyles.row }>
+          <CalculatorButton label="7" />
+          <CalculatorButton label="8" />
+          <CalculatorButton label="9" />
+          <CalculatorButton label="x" color={ colors.orange } />
+        </View>
+
+        <View style={ globalStyles.row }>
+          <CalculatorButton label="4" />
+          <CalculatorButton label="5" />
+          <CalculatorButton label="6" />
+          <CalculatorButton label="-" color={ colors.orange } />
+        </View>
+
+        <View style={ globalStyles.row }>
+          <CalculatorButton label="1" />
+          <CalculatorButton label="2" />
+          <CalculatorButton label="3" />
+          <CalculatorButton label="+" color={ colors.orange } />
+        </View>
+
+        <View style={ globalStyles.row }>
+          <CalculatorButton label="0" doubleSize />
+          <CalculatorButton label="." />
+          <CalculatorButton label="=" color={ colors.orange } />
+        </View>
+
     </View>
   )
 }
